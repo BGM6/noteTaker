273273abd31c788b72df5af2a382fb722d98babd
bguilas <bryanjguilas@gmail.com>
2020-10-18 20:42:06 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+    # My-Note-Taker 
+    A simple web application for note taking.
+
+    # Installation
+    In the terminal run 'npm install'
+    # Technology
+    javascript, express, node.js, css, html
+    # Author 
+    
+**bgm6**
+    
+Email: bguilas21@yahoo.com
+    
+GitHub: https://github.com/BGM6
+
+![alt text](/screenShot/notTakerScreenShot.png "HTML Screenshot")
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
+var PORT = process.env.PORT || 1080;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use(htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Listening to port: ${PORT}`);
+});
